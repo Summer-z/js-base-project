512d57ac8cc0ccbac9ea01874ae8248ba6f66b27
Summer-z <979971754@qq.com>
2017-08-10 22:40:08 +0800

    new_task7

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -1,31 +1,48 @@
 "use strict";
-var _ = require("lodash");
-var chai = require("chai");
-var sinon = require("sinon");
-var sinonChai = require("sinon-chai");
-var expect = chai.expect;
+let _ = require("lodash");
+let chai = require("chai");
+let sinon = require("sinon");
+let sinonChai = require("sinon-chai");
+let expect = chai.expect;
 chai.use(sinonChai);
 
-var main = require("../lib/main.js");
+let getEntireLyrics = require("../lib/main.js");
+let lyrics = require("../lib/lyrics.js");
 
-
-describe("测试描述", function(){
+describe("Print out the lyrics of '99 bottles of beer on the wall' song", function(){
     sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
-
-        var result = main();
-        var expect_string = '';
-        
-        expect(expect_string).to.equal(result);
+    it("Test the last paragraph of the lyrics", function(){
+        let num = 0;
+        let result = getEntireLyrics(num);
+        let expect_string = 'No more bottles of beer on the wall, no more bottles of beer.\n'+
+            'Go to the store and buy some more, 99 bottles of beer on the wall.';
+        expect(expect_string).to.be.equal(result);
     });
 
-    it("测试用例2", function(){
-
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
-
-        expect(expect_string).to.equal(result);
+    it("Test the last two paragraph of the lyrics", function(){
+        let num = 1;
+        let result = getEntireLyrics(num);
+        let expect_string = '1 bottle of beer on the wall, 1 bottle of beer.\n'+
+            'Take one down and pass it around, no more bottles of beer on the wall.\n'+
+            'No more bottles of beer on the wall, no more bottles of beer.\n'+
+            'Go to the store and buy some more, 99 bottles of beer on the wall.';
+        expect(expect_string).to.be.equal(result);
     });
+
+    it("Test the entire lyrics of the song '99 bottles of beer on the wall'", function(){
+        let num = 99;
+        let result = getEntireLyrics(num);
+        let expect_string = lyrics();
+        expect(expect_string).to.be.equal(result);
+    });
+
+    // it("测试用例2", function(){
+    //
+    //     main();
+    //     let result = _.flatten(console.log.args).join("\n");
+    //     let expect_string = '';
+    //
+    //     expect(expect_string).to.equal(result);
+    // });
 });
